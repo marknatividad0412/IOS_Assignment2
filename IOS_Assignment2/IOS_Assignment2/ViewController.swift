3f54881357a56424df5c98ebf1d554a317172ef3
Mark <marknatividad0412@gmail.com>
2020-05-15 07:29:54 +1000

    This is my initial file

---
+++ IOS_Assignment2/IOS_Assignment2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS_Assignment2
+//
+//  Created by Mark Anthony Natividad on 15/5/20.
+//  Copyright © 2020 Mark Anthony Natividad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
