ce1c5318f6f62e0c34188efac6536f3ac9d3addb
Mark <marknatividad0412@gmail.com>
2020-05-16 12:19:05 +1000

    Addew ViewControllers

---
+++ IOS_Assignment2/IOS_Assignment2/NewGameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewGameViewController.swift
+//  IOS_Assignment2
+//
+//  Created by Mark Anthony Natividad on 16/5/20.
+//  Copyright © 2020 Mark Anthony Natividad. All rights reserved.
+//
+
+import UIKit
+
+class NewGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
