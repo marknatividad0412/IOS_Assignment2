a63979b53809147f5c9745304ca19dcef87c7a3c
Mark <marknatividad0412@gmail.com>
2020-05-17 20:57:35 +1000

    Table View inserted

--- IOS_Assignment2/IOS_Assignment2/HighScoresViewController.swift
+++ IOS_Assignment2/IOS_Assignment2/HighScoresViewController.swift
@@ -10,21 +10,34 @@ import UIKit
 
 class HighScoresViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.delegate = self
+        
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
+   
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+extension HighScoresViewController: UITableViewDelegate{
+       func tableView (_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+           
+       }
+   }
+extension HighScoresViewController:UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 3
     }
-    */
-
+    func tableView( _ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HighScoresList", for: indexPath)
+        
+        cell.textLabel?.text = "Hello World"
+        return cell
+    }
 }
